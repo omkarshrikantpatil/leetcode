fb8363b4f494446b2cc62cefbaa1640bec91fd44
omkar <omkarshrikantpatil@gmail.com>
2025-04-30 11:05:36 +0530

    leetcode day 3

---
+++ LeetCode1941.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class LeetCode1941 {
+    public static boolean checkCount(String myString){
+        HashMap<Character, Integer> mapCounts = new HashMap<>();
+        for(char c: myString.toCharArray()){
+            mapCounts.put(c, mapCounts.getOrDefault(c, 0) + 1);
+        }
+        int frequency = mapCounts.values().iterator().next();
+        for(int count: mapCounts.values()){
+            if(count != frequency){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
