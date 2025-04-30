19954003c05ad37253faed00868d957ec1f1f4dc
omkar <omkarshrikantpatil@gmail.com>
2025-04-30 07:59:18 +0530

    leetcode day 3

--- LeetCode1945.java
+++ LeetCode1945.java
@@ -2,15 +2,32 @@ import java.util.HashMap;
 
 public class LeetCode1945 {
     public static int characterSum(String s, int k){
-        HashMap<Character, Integer> map = new HashMap<>();
-        for(char c = 'a'; c <= 'z'; c++){
-            map.put(c, c - 'a' + 1);
+//        HashMap<Character, Integer> map = new HashMap<>();
+//        for(char c = 'a'; c <= 'z'; c++){
+//            map.put(c, c - 'a' + 1);
+//        }
+//        StringBuilder str = new StringBuilder();
+//        for(char c : s.toCharArray()){
+//            str.append(map.get(c));
+//        }
+//        String current = str.toString();
+//        for(int i=0; i<k; i++){
+//            int sum = 0;
+//            for(char digit: current.toCharArray()){
+//                sum += digit - '0';
+//            }
+//            current = Integer.toString(sum);
+//        }
+//        return Integer.parseInt(current);
+
+        //Using another approach
+        String numStr = "";
+        for(char c: s.toCharArray()){
+            int val = c - 'a' + 1;
+            numStr += val;
         }
-        StringBuilder str = new StringBuilder();
-        for(char c : s.toCharArray()){
-            str.append(map.get(c));
-        }
-        String current = str.toString();
+
+        String current = numStr;
         for(int i=0; i<k; i++){
             int sum = 0;
             for(char digit: current.toCharArray()){
@@ -21,7 +38,7 @@ public class LeetCode1945 {
         return Integer.parseInt(current);
     }
     public static void main(String[] args) {
-        int charSum = characterSum("Omkar", 2);
+        int charSum = characterSum("abcd", 2);
         System.out.println(charSum);
     }
 }
